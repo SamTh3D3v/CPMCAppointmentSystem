78918c409bfd84b8475730c5e48280bf1f09006a
samthedev <abdelkhalekdev@gmail.com>
2015-08-10 00:48:24 +0100

    the services skeleton

---
+++ ServiceLayer/CpmcService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceLayer
+{    
+    public class CpmcService : IUserService,IAdminService,IDoctorService,IAuthentificationService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
