fe654add77c88be802a9c400431a8da9d32f6160
samthedev <abdelkhalekdev@gmail.com>
2015-08-20 04:13:06 +0100

    Set the ScheduleView

--- CPMCAppointmentSystem/View/CalendarView.xaml.cs
+++ CPMCAppointmentSystem/View/CalendarView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Syncfusion.UI.Xaml.Schedule;
 
 namespace CPMCAppointmentSystem.View
 {
@@ -23,6 +24,38 @@ namespace CPMCAppointmentSystem.View
         public CalendarView()
         {
             InitializeComponent();
+        }
+
+        private void Btn_ScheduleType_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as RadioButton).Name)
+            {
+                case "Day":
+                    {
+                        Schedule.ScheduleType = ScheduleType.Day;
+                        break;
+                    }
+                case "Week":
+                    {
+                        Schedule.ScheduleType = ScheduleType.Week;
+                        break;
+                    }
+                case "WorkWeek":
+                    {
+                        Schedule.ScheduleType = ScheduleType.WorkWeek;
+                        break;
+                    }
+                case "Month":
+                    {
+                        Schedule.ScheduleType = ScheduleType.Month;
+                        break;
+                    }
+                case "TimeLine":
+                    {
+                        Schedule.ScheduleType = ScheduleType.TimeLine;
+                        break;
+                    }
+            }
         }
     }
 }
