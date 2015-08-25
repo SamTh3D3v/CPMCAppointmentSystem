97c984b83d21762e63497926744ad8cccb5b89d6
samthedev <abdelkhalekdev@gmail.com>
2015-08-25 11:31:07 +0100

    Update Specilite Table

--- DataLayer/Model/Specialite.cs
+++ DataLayer/Model/Specialite.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataLayer.Model
 {
+    [Table("Specialite")]
     public class Specialite
     {
         public Specialite()
         {
             
         }
-
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid SpecialiteId { get; set; }
+        
+        [Required]
+        public String Name { get; set; }
+        public String Description { get; set; }
+        
         public virtual ICollection<Medecin> Medecins { get; set; }
 
     }
