3f3a7eb441c97126c9b086e0a7842a14c8a72bef
samthedev <abdelkhalekdev@gmail.com>
2015-09-06 07:19:54 +0100

    Basic Pathology View and ViewModel

--- CPMCAppointmentSystem/ViewModel/PathologiesViewModel.cs
+++ CPMCAppointmentSystem/ViewModel/PathologiesViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CPMCAppointmentSystem.Helpers;
 using DataLayer.Model;
+using GalaSoft.MvvmLight.Command;
 using Syncfusion.Windows.Forms.Tools.Navigation;
 
 namespace CPMCAppointmentSystem.ViewModel
@@ -14,6 +15,7 @@ namespace CPMCAppointmentSystem.ViewModel
         #region Fields
         private ObservableCollection<Pathology> _pathologiesList;
         private Pathology _selectedPathology;
+        private Medecin _selectedDoctorWithinPathology;
         #endregion
         #region Properties
         public ObservableCollection<Pathology> PathologiesList
@@ -52,8 +54,104 @@ namespace CPMCAppointmentSystem.ViewModel
                 RaisePropertyChanged();
             }
         }
+        public Medecin SelectedDoctorWithinPathology
+        {
+            get
+            {
+                return _selectedDoctorWithinPathology;
+            }
+
+            set
+            {
+                if (_selectedDoctorWithinPathology == value)
+                {
+                    return;
+                }
+
+                _selectedDoctorWithinPathology = value;
+                RaisePropertyChanged();
+            }
+        }
         #endregion
         #region Commands
+        private RelayCommand _pathologyViewLoadedCommand;
+        public RelayCommand PathologyViewLoadedCommand
+        {
+            get
+            {
+                return _pathologyViewLoadedCommand
+                    ?? (_pathologyViewLoadedCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
+        }
+        private RelayCommand _addPathologyCommand;
+        public RelayCommand AddPathologyCommand
+        {
+            get
+            {
+                return _addPathologyCommand
+                    ?? (_addPathologyCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
+        }
+        private RelayCommand _addDoctorToPathologyCommand;
+        public RelayCommand AddDoctorToPathologyCommand
+        {
+            get
+            {
+                return _addDoctorToPathologyCommand
+                    ?? (_addDoctorToPathologyCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
+        }
+        private RelayCommand _savePathologyCommand;
+        public RelayCommand SavePathologyCommand
+        {
+            get
+            {
+                return _savePathologyCommand
+                    ?? (_savePathologyCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
+        }
+        private RelayCommand _deletePathologyCommand;
+        public RelayCommand DeletePathologyCommand
+        {
+            get
+            {
+                return _deletePathologyCommand
+                    ?? (_deletePathologyCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
+        }
+        private RelayCommand _cancelChangesToPathologyCommand;
+        public RelayCommand CancelChangesToPathologyCommand
+        {
+            get
+            {
+                return _cancelChangesToPathologyCommand
+                    ?? (_cancelChangesToPathologyCommand = new RelayCommand(
+                    () =>
+                    {
+                        
+                    }));
+            }
+        }
         
         #endregion
         #region Ctors and Methods
