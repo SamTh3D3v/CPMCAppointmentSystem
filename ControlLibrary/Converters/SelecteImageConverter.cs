704554d6751e40d70d37f1f341624e12b43bf75d
samthedev <abdelkhalekdev@gmail.com>
2015-09-30 17:47:04 +0100

    Turn The NotificationSent Bool into an icon

---
+++ ControlLibrary/Converters/SelecteImageConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ControlLibrary.Converters
+{
+    public class SelecteImageConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool) value)
+                ? "/ControlLibrary;component/Images/Icons/Dark/SendYes.png"
+                : "/ControlLibrary;component/Images/Icons/Dark/SendNo.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
