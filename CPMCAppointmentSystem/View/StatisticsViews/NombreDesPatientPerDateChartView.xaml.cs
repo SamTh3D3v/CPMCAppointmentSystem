750d759145fef3e7377081551e46377ca31a3099
samthedev <abdelkhalekdev@gmail.com>
2015-10-08 23:26:06 +0100

    Add main Statistics Views

---
+++ CPMCAppointmentSystem/View/StatisticsViews/NombreDesPatientPerDateChartView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPMCAppointmentSystem.View.StatisticsViews
+{
+    /// <summary>
+    /// Interaction logic for NombreDesPatientPerDateChartView.xaml
+    /// </summary>
+    public partial class NombreDesPatientPerDateChartView : UserControl
+    {
+        public NombreDesPatientPerDateChartView()
+        {
+            InitializeComponent();
+        }
+    }
+}
