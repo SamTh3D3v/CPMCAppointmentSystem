13c0c24a67d5235134e7f3cfec912901d9ea14dc
samthedev <abdelkhalekdev@gmail.com>
2015-10-08 23:45:41 +0100

    Add Statistics Views Corresponding ViewModel

---
+++ CPMCAppointmentSystem/ViewModel/StatisticsViewModels/PatientPerAgeChartViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CPMCAppointmentSystem.Helpers;
+using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace CPMCAppointmentSystem.ViewModel.StatisticsViewModels
+{
+    public class PatientPerAgeChartViewModel : StatisticsChartsViewModelBase
+    {
+         #region Fields
+
+        #endregion
+        #region Properties
+
+        #endregion
+        #region Commands      
+
+        #endregion
+        #region Ctors Methods
+        public PatientPerAgeChartViewModel(IFrameNavigationService mainFrameNavigationService, IInnerFrameNavigationService innerFrameNavigationService)
+            : base(mainFrameNavigationService, innerFrameNavigationService)
+        {
+        }
+        #endregion
+    }
+}
