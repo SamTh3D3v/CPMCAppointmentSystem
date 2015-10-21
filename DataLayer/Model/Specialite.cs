8c54590aaed1b5acf26fd515f45b378b4acbfc05
samthedev <abdelkhalekdev@gmail.com>
2015-10-21 15:00:27 +0100

    Update the Specialite Model To through Validation Exception

--- DataLayer/Model/Specialite.cs
+++ DataLayer/Model/Specialite.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -12,7 +13,7 @@ using DataLayer.Annotations;
 namespace DataLayer.Model
 {
     [Table("Specialite")]
-    public class Specialite : INotifyPropertyChanged
+    public class Specialite : INotifyPropertyChanged, IDataErrorInfo
     {      
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -22,9 +23,8 @@ namespace DataLayer.Model
         public String Name { get; set; }
         [Required]
         public String Code { get; set; }
-        public String Description { get; set; }
-        
-        public virtual ICollection<Medecin> Medecins { get; set; }
+        public String Description { get; set; }     
+        public virtual ObservableCollection<Medecin> Medecins { get; set; }
         
         #region INotifyPropertyChanged related
 
@@ -37,5 +37,27 @@ namespace DataLayer.Model
             if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
         #endregion
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = null;
+                if (columnName == "Name")
+                {
+                    if (string.IsNullOrEmpty(Name))
+                        result = "Spesifiez le nom du specialité";
+                }
+                if (columnName == "Code")
+                {
+                    if (string.IsNullOrEmpty(Code))
+                        result = "Spesifiez le code du specialité";
+                }
+                return result;
+            }
+        }
+        public string Error
+        {
+            get { return String.Empty; }
+        }
     }
 }
