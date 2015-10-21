8ee12fed785b1626f2c9d72379d67a842f642f4c
samthedev <abdelkhalekdev@gmail.com>
2015-10-21 15:10:50 +0100

    Move the MaskedEdit Style To The Control Library

--- CPMCAppointmentSystem/View/PathologiesViews/PathologiesView.xaml.cs
+++ CPMCAppointmentSystem/View/PathologiesViews/PathologiesView.xaml.cs
@@ -6,19 +6,10 @@ namespace CPMCAppointmentSystem.View.PathologiesViews
     /// Interaction logic for PathologiesView.xaml
     /// </summary>
     public partial class PathologiesView : Page
-    {
-        private int _errorsCount;
+    {        
         public PathologiesView()
         {
             InitializeComponent();
-        }
-
-        private void NomBox_OnError(object sender, ValidationErrorEventArgs e)
-        {
-            if (e.Action == ValidationErrorEventAction.Added)
-                _errorsCount++;
-            else
-                _errorsCount--;
         }
     }
 }
---
+++ CPMCAppointmentSystem/View/SettingsViews/SettingsView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using GalaSoft.MvvmLight.Messaging;
+
+namespace CPMCAppointmentSystem.View.SettingsViews
+{    
+    public partial class SettingsView : Page
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+            Messenger.Default.Register<NotificationMessage>(this, (m) =>
+            {
+                switch (m.Notification)
+                {
+                    case "Refresh":
+                        ReportPreviewer.RefreshReport();     
+                        break;
+                }
+            });
+        }
+
+       
+    }
+}
