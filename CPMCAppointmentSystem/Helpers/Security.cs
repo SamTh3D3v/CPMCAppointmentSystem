2094080c4bd591e7f9d6dfc25d4c0219bc42b7ec
samthedev <abdelkhalekdev@gmail.com>
2015-10-22 20:31:23 +0100

    Add the Hash Class To Store Passes In Sha1

---
+++ CPMCAppointmentSystem/Helpers/Security.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPMCAppointmentSystem.Helpers
+{
+    public class Security
+    {
+        public static string HashSha1(string value)
+        {
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            foreach (byte t in hash)
+            {
+                sb.Append(t.ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
