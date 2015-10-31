40cc3d93eeac38c7edfbaa88b8a8608c517b64c6
Abdelkhalek Oussama Elhamer <oussama.elhamer@outlook.com>
2015-10-31 14:03:00 +0100

    Set the log UI

--- CPMCAppointmentSystem/ViewModel/LogViewModel.cs
+++ CPMCAppointmentSystem/ViewModel/LogViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,46 @@ namespace CPMCAppointmentSystem.ViewModel
     public class LogViewModel : NavigableViewModelBase
     {
         #region Fileds
-        
+        private string _selectedLog;
+        private ObservableCollection<string> _logCollectionList; 
         #endregion
         #region Properties
+        public string SelectedLog
+        {
+            get
+            {
+                return _selectedLog;
+            }
+
+            set
+            {
+                if (_selectedLog == value)
+                {
+                    return;
+                }
+
+                _selectedLog = value;
+                RaisePropertyChanged();
+            }
+        }               
+        public ObservableCollection<string> LogCollectionList
+        {
+            get
+            {
+                return _logCollectionList;
+            }
+
+            set
+            {
+                if (_logCollectionList == value)
+                {
+                    return;
+                }
+
+                _logCollectionList = value;
+                RaisePropertyChanged();
+            }
+        }
         
         #endregion 
         #region Commands
