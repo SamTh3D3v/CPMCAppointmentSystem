aa44c973c66d2cc13fa3eab4dc4537a320c9ba52
Abdelkhalek Oussama Elhamer <abdelkhalekdev@gmail.com>
2015-11-03 19:39:17 +0100

    Fix the Change Password Logic In the Settings View

--- CPMCAppointmentSystem/View/SettingsViews/SettingsView.xaml.cs
+++ CPMCAppointmentSystem/View/SettingsViews/SettingsView.xaml.cs
@@ -5,11 +5,12 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using DataLayer.Model;
 using GalaSoft.MvvmLight.Messaging;
+using Syncfusion.UI.Xaml.Grid;
 using Syncfusion.Windows.Converters;
 using Path = System.Windows.Shapes.Path;
 
 namespace CPMCAppointmentSystem.View.SettingsViews
-{    
+{
     public partial class SettingsView : Page
     {
         public SettingsView()
@@ -19,21 +20,40 @@ namespace CPMCAppointmentSystem.View.SettingsViews
             {
                 switch (m.Notification)
                 {
-                    case "Refresh":
-                        ReportPreviewer.RefreshReport();     
+                    case "Refresh   ":
+                        ReportPreviewer.RefreshReport();
                         break;
                 }
             });
-                        
+
         }
 
         private void UserPass_OnPasswordChanged(object sender, RoutedEventArgs e)
         {
+            if (PassCheckBox.IsChecked == true) { 
             if (!String.IsNullOrEmpty(UserPass.Password) && UserPass.Password.Equals(UserConfirmPass.Password))
                 BtnSave.Tag = "yes";
             else
                 BtnSave.Tag = "no";
+            }
+        }
 
+        private void SfDataGrid_OnSelectionChanged(object sender, GridSelectionChangedEventArgs e)
+        {
+            PassCheckBox.IsChecked = false;
+            BtnSave.Tag = "yes";
+            UserPass.Clear();
+            UserConfirmPass.Clear();
+        }
+
+        private void PassCheckBox_OnUnChecked(object sender, RoutedEventArgs e)
+        {            
+                BtnSave.Tag = "yes";
+        }
+
+        private void PassCheckBox_OnChecked(object sender, RoutedEventArgs e)
+        {
+            BtnSave.Tag = "no";
         }
     }
 }
