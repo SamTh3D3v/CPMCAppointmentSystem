088c2e1356d8892fe57b496f652dd2087f5e8ea9
f-belhocine <farouk.belhocine@gmail.com>
2015-11-05 14:59:17 +0100

    Adding TypeUserUtility and adding TypeUser.None in TypeUser enum

--- DataLayer/Enums/TypeUser.cs
+++ DataLayer/Enums/TypeUser.cs
@@ -5,6 +5,7 @@ namespace DataLayer.Model
     [Flags]
     public enum TypeUser
     {
+        None=0x0,
         Agent=0x1,
         Medecin =0x2,
         Admin=0x4,
---
+++ DataLayer/Helpers/TypeUserUtility.cs
@@ -0,0 +1,41 @@
+﻿using DataLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public static class TypeUserUtility
+    {
+        public static bool IsAgent(TypeUser typeUser)
+        {
+            return TypeUser.Agent == (TypeUser.Agent & typeUser);
+        }
+        public static bool IsMedecin(TypeUser typeUser)
+        {
+            return TypeUser.Medecin == (TypeUser.Medecin & typeUser);
+        }
+        public static bool IsAdmin(TypeUser typeUser)
+        {
+            return TypeUser.Admin == (TypeUser.Admin & typeUser);
+        }
+        public static TypeUser WhichTypeUser(bool isAdmin, bool isMedecin, bool isAgent)
+        {
+            TypeUser typeUser = TypeUser.None;
+
+            if (isAdmin)
+                typeUser = TypeUser.Admin | typeUser;
+
+            if (isMedecin)
+                typeUser = TypeUser.Medecin | typeUser;
+
+            if (isAgent)
+                typeUser = TypeUser.Agent | typeUser;
+
+            return typeUser;
+         }
+
+    }
+}
