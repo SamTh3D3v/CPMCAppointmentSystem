3eaf9f1fa0d2824960955281f36ad5ced54191d3
f-belhocine <farouk.belhocine@gmail.com>
2015-11-07 22:09:39 +0100

    Adding Parameter entity and ParameterNames enum to the Model

---
+++ DataLayer/Entities/Parameter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Model
+{
+    public class Parameter
+    {
+        public Guid Id { get; set; }
+
+        [MaxLength(128)]
+        public string Name { get; set; }
+        [MaxLength(256)]
+        public string Title { get; set; }
+        [MaxLength(256)]
+        public string Value { get; set; }
+
+        public bool SyncOnChange { get; set; }
+    }
+}
---
+++ DataLayer/Enums/ParameterNames.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Model
+{
+    public enum ParameterNames
+    {
+        RemindPatientRDVBefore, // int - Days
+        SMSNotSendPatientRDVSince, // int - Days
+        PatientNotConfirmRDVSince, // int - Days
+        PatientWihoutRDVSince, // int - Days
+        SMSCenterNumber, // string - Sync on change
+        SMSBodyTemplate, // string - Sync on change
+        DelayBetweenATCommand,// int - Sync on change
+        MaxNumberOfRetryAfterSMSSendFailure // int - Sync on change        
+    }
+}
