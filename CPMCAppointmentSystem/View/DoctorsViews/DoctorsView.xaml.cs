d0272c03b3cb6a669bf9eb1d3b6505fd06a3a21c
Abdelkhalek Oussama Elhamer <abdelkhalekdev@gmail.com>
2015-11-07 19:35:32 +0100

    Save and update pass on the doctors view logic

--- CPMCAppointmentSystem/View/DoctorsViews/DoctorsView.xaml.cs
+++ CPMCAppointmentSystem/View/DoctorsViews/DoctorsView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace CPMCAppointmentSystem.View.DoctorsViews
 {
@@ -10,6 +12,26 @@ namespace CPMCAppointmentSystem.View.DoctorsViews
         public DoctorsView()
         {
             InitializeComponent();            
+        }
+
+        private void UserPass_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (CbUpdatePass.IsChecked == true)
+            {
+                if (!String.IsNullOrEmpty(PassBoxUserPass.Password) && PassBoxUserPass.Password.Equals(PassBoxConfirmPass.Password))
+                    BuSave.Tag = "yes";
+                else
+                    BuSave.Tag = "no";
+            }
+        }
+        private void PassCheckBox_OnUnChecked(object sender, RoutedEventArgs e)
+        {
+            BuSave.Tag = "yes";
+        }
+
+        private void PassCheckBox_OnChecked(object sender, RoutedEventArgs e)
+        {
+            BuSave.Tag = "no";
         }
     }
 }
