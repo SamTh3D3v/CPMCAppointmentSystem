cf5ee74f184b6c660be8fedaba8e35ca274fdc5c
f-belhocine <farouk.belhocine@gmail.com>
2015-11-08 16:24:11 +0100

    Adding Sync Notifier when SyncOnChange parameters change. using SyncedParameterChange event.

--- DataLayer/Notifications/NotificationHelper.cs
+++ DataLayer/Notifications/NotificationHelper.cs
@@ -14,28 +14,83 @@ namespace DataLayer.Notifications
         #region Fields
 
         private SqlConnection _connection;
-        private SqlCommand _command;
-        private SqlDependency _dependency;
+        private SqlCommand _notificationsCommand;
+        private SqlCommand _parametersCommand;
+        private SqlDependency _notificationsDependency;
+        private SqlDependency _parametersDependency;
+
         private string _connectionString;
         private Guid _userId;
+        private bool _started;
 
-        #endregion      
+        #endregion
         public NotificationHelper()
         {
             _connectionString = GetConnectionString();
             _connection = _connection ?? new SqlConnection(_connectionString);
-            _command = _command ?? new SqlCommand(@"usp_GetNotifications", _connection);
-            _command.CommandType = System.Data.CommandType.StoredProcedure;
 
-           
+            // initialize notifications command.
+            _notificationsCommand = _notificationsCommand ?? new SqlCommand(@"usp_GetNotifications", _connection);
+            _notificationsCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            _notificationsCommand.Notification = null;
 
-            _command.Notification = null;
+            // initialize parameters command.
+            _parametersCommand = _parametersCommand ?? new SqlCommand(@"usp_GetSyncedParameters", _connection);
+            _parametersCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            _parametersCommand.Notification = null;
         }
         public void Start()
-        {                       
-            // Starting SQL Server Query Notifications.
-            SqlDependency.Stop(_connectionString);
-            SqlDependency.Start(_connectionString);            
+        {
+            if (!_started)
+            {
+                _notificationsCommand.Parameters.AddWithValue("@UserId", DBNull.Value);
+
+                // Starting SQL Server Query Notifications.
+                SqlDependency.Stop(_connectionString);
+                SqlDependency.Start(_connectionString);
+
+                _started = true;// IMPORTANT
+            }
+
+            // Get the Initial Notifications Result set, after which changes will be tracked.
+            List<Notification> initalNotificationsData = GetNotifications();
+
+            // Firing Event to be Handled in UI App.
+            if (NotificationsChange != null)
+            {
+                var args = new NotificationEventArgs<Notification>(initalNotificationsData);
+
+                NotificationsChange(this, args);
+            }
+
+            // Get the Initial Synced Parameters.
+            List<Parameter> initialSyncedParametersData = GetSyncedParameters();
+
+            // Firing Event to be Handled in UI App.
+            if (SyncedParametersChange != null)
+            {
+                var args = new NotificationEventArgs<Parameter>(initialSyncedParametersData);
+                SyncedParametersChange(this, args);
+            }
+
+        }
+        public void Start(Guid currentUserId)
+        {
+            if (!_started)
+            {
+                _userId = currentUserId;
+
+                if (_userId == Guid.Empty)
+                    _notificationsCommand.Parameters.AddWithValue("@UserId", DBNull.Value);
+                else
+                    _notificationsCommand.Parameters.AddWithValue("@UserId", _userId);
+
+                // Starting SQL Server Query Notifications.
+                SqlDependency.Stop(_connectionString);
+                SqlDependency.Start(_connectionString);
+
+                _started = true;// IMPORTANT
+            }
 
             // Get the Initial Notifications Result set, after which changes will be tracked.
             List<Notification> initalNotificationsData = GetNotifications();
@@ -47,31 +102,16 @@ namespace DataLayer.Notifications
 
                 NotificationsChange(typeof(NotificationHelper), args);
             }
-        }
 
-        public void Start(Guid currentUserId)
-        {
-            _userId = currentUserId;
-
-            if (_userId == Guid.Empty)
-                _command.Parameters.AddWithValue("@UserId", DBNull.Value);
-            else
-                _command.Parameters.AddWithValue("@UserId", _userId);
-
-            // Starting SQL Server Query Notifications.
-            SqlDependency.Stop(_connectionString);
-            SqlDependency.Start(_connectionString);
-
-            // Get the Initial Notifications Result set, after which changes will be tracked.
-            List<Notification> initalNotificationsData = GetNotifications();
+            // Get the Initial Synced Parameters.
+            List<Parameter> initialSyncedParametersData = GetSyncedParameters();
 
             // Firing Event to be Handled in UI App.
-            if (NotificationsChange != null)
+            if (SyncedParametersChange != null)
             {
-                var args = new NotificationEventArgs<Notification>(initalNotificationsData);
-
-                NotificationsChange(typeof(NotificationHelper), args);
-            }            
+                var args = new NotificationEventArgs<Parameter>(initialSyncedParametersData);
+                SyncedParametersChange(this, args);
+            }
         }
         public void Stop()
         {
@@ -79,27 +119,11 @@ namespace DataLayer.Notifications
             SqlDependency.Stop(GetConnectionString());
         }
 
-        public event NotificationEventHandler<Notification> NotificationsChange;        
+        public event NotificationEventHandler<Notification> NotificationsChange;
 
-        #region Helper Methods 
-        private void OnChange(object sender, SqlNotificationEventArgs e)
-        {
-            SqlDependency dependency = (SqlDependency)sender;
+        public event NotificationEventHandler<Parameter> SyncedParametersChange;
 
-            // Unregister dependency.
-            dependency.OnChange -= this.OnChange;
-
-            // Get new Notifications Data (After Changes Done).
-            List<Notification> newResult = GetNotifications();
-
-            // Firing Event to be Handled in UI App.
-            if (NotificationsChange != null)
-            {
-                var args = new NotificationEventArgs<Notification>(newResult);
-
-                NotificationsChange(typeof(NotificationHelper), args);
-            }
-        }
+        #region Helper Methods
         private string GetConnectionString()
         {
             string connectionString = ConfigurationManager.ConnectionStrings["CpmcContext"].ConnectionString;
@@ -110,16 +134,16 @@ namespace DataLayer.Notifications
         {
             List<Notification> newResult = new List<Notification>();
 
-            _command.Notification = null;
-            
+            _notificationsCommand.Notification = null;
+
             // Register Dependency with the Command.
-            _dependency = new SqlDependency(_command);            
-            _dependency.OnChange += this.OnChange;
-                  
+            _notificationsDependency = new SqlDependency(_notificationsCommand);
+            _notificationsDependency.OnChange += this.OnNotificationsChange;
+
 
             // Get Actual Notifications
             _connection.Open();
-            using (var reader = _command.ExecuteReader())
+            using (var reader = _notificationsCommand.ExecuteReader())
             {
                 while (reader.Read())
                 {
@@ -137,13 +161,81 @@ namespace DataLayer.Notifications
                         IsActive = (bool)reader["IsActive"]
                     });
                 }
-            } 
-                    
+            }
+
             _connection.Close();
 
             return newResult;
         }
-        
+        private List<Parameter> GetSyncedParameters()
+        {
+            List<Parameter> newResult = new List<Parameter>();
+
+            _parametersCommand.Notification = null;
+
+            // Register Dependency with the Command.
+            _parametersDependency = new SqlDependency(_parametersCommand);
+            _parametersDependency.OnChange += this.OnSyncedParametersChange;
+
+
+            // Get Actual synced parameters.
+            _connection.Open();
+            using (var reader = _parametersCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    newResult.Add(new Parameter
+                    {
+                        Id = (Guid)reader["Id"],
+                        Name = (string)reader["Name"],
+                        Title = (string)reader["Title"],
+                        Value = (string)reader["Value"],
+                        SyncOnChange = (bool)reader["SyncOnChange"]
+                    });
+                }
+            }
+
+            _connection.Close();
+
+            return newResult;
+        }
+        private void OnSyncedParametersChange(object sender, SqlNotificationEventArgs e)
+        {
+            SqlDependency dependency = (SqlDependency)sender;
+
+            // Unregister dependency.
+            dependency.OnChange -= this.OnSyncedParametersChange;
+
+            // Get new Notifications Data (After Changes Done).
+            List<Parameter> newResult = GetSyncedParameters();
+
+            // Firing Event to be Handled in UI App.
+            if (SyncedParametersChange != null)
+            {
+                var args = new NotificationEventArgs<Parameter>(newResult);
+
+                SyncedParametersChange(this, args);
+            }
+        }
+        private void OnNotificationsChange(object sender, SqlNotificationEventArgs e)
+        {
+            SqlDependency dependency = (SqlDependency)sender;
+
+            // Unregister dependency.
+            dependency.OnChange -= this.OnNotificationsChange;
+
+            // Get new Notifications Data (After Changes Done).
+            List<Notification> newResult = GetNotifications();
+
+            // Firing Event to be Handled in UI App.
+            if (NotificationsChange != null)
+            {
+                var args = new NotificationEventArgs<Notification>(newResult);
+
+                NotificationsChange(typeof(NotificationHelper), args);
+            }
+        }
+
         #endregion
     }
 }
