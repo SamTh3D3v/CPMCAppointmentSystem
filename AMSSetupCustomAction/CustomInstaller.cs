759ad022cc4c39e17b4f37622b0e87d2fe0e7da9
f-belhocine <farouk.belhocine@gmail.com>
2015-11-11 10:59:52 +0100

    Adding Setup and Setpu Custom Actions projects.

---
+++ AMSSetupCustomAction/CustomInstaller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMSSetupCustomAction
+{
+    [RunInstaller(true)]
+    public partial class CustomInstaller : System.Configuration.Install.Installer
+    {
+        public CustomInstaller()
+        {
+            InitializeComponent();
+        }
+
+
+        public override void Install(IDictionary stateSaver)
+        {
+
+
+            base.Install(stateSaver);
+
+            // Get actual configuration
+            Configuration config = ConfigurationManager.OpenExeConfiguration(this.Context.Parameters["targetDir"].ToString() + "CPMCAppointmentSystem.exe");
+
+            // Get Connection String Section.
+            ConnectionStringsSection connectionStringSection = config.ConnectionStrings;
+
+            // Encrypt Connection String Section.
+            if (!connectionStringSection.SectionInformation.IsProtected)
+                connectionStringSection.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
+
+            config.Save();
+        }
+    }
+}
