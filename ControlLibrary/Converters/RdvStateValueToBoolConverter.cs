7e00682a9d1f11bb4d7ff9899ce39201df0c589d
Abdelkhalek Oussama Elhamer <abdelkhalekdev@gmail.com>
2015-11-16 08:23:49 +0100

    Annuler/Activer le RDV From the Form

---
+++ ControlLibrary/Converters/RdvStateValueToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using DataLayer.Enums;
+
+namespace ControlLibrary.Converters
+{
+    public class RdvStateValueToBoolConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            return (RdvState) value == RdvState.NotYet;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            return ((bool) value) ? RdvState.NotYet : RdvState.Cancelled;
+        }
+    }
+}
