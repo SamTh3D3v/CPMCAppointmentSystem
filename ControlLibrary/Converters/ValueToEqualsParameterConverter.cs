e5a94dac50d79566e1cc505fd8707389551a10c5
Abdelkhalek Oussama Elhamer <abdelkhalekdev@gmail.com>
2015-12-25 16:52:15 +0100

    Fix the selected UserType Id Icon

--- ControlLibrary/Converters/ValueToEqualsParameterConverter.cs
+++ ControlLibrary/Converters/ValueToEqualsParameterConverter.cs
@@ -12,8 +12,20 @@ namespace ControlLibrary.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value == null) return null;            
+            if (value == null) return null;           
             return value.Equals(parameter);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    } 
+    public class IdEqualsParameterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;           
+            return int.Parse(value.ToString())==int.Parse(parameter.ToString());
         }
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
