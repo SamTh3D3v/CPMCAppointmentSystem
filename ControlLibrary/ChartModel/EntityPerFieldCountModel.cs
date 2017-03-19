d33d6f29e9b2f1155ae8df0a9cbaeb5172c57139
Abdelkhalek Oussama Elhamer <Abdelkhalekdev@gmail.com>
2017-03-19 22:57:22 +0100

    Fix "Patients per date" chart

--- ControlLibrary/ChartModel/EntityPerFieldCountModel.cs
+++ ControlLibrary/ChartModel/EntityPerFieldCountModel.cs
@@ -1,15 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using DataLayer.Annotations;
 
 namespace ControlLibrary.ChartModel
 {
-    public class EntityPerFieldCountModel
+    public class EntityPerFieldCountModel:INotifyPropertyChanged
     {
-        public string Field { get; set; }
-        public double Count { get; set; }
+        private string _field;
+        private double _count;
+
+        public string Field
+        {
+            get { return _field; }
+            set
+            {
+                if (value == _field) return;
+                _field = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Count
+        {
+            get { return _count; }
+            set
+            {
+                if (value.Equals(_count)) return;
+                _count = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
 
